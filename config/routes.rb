b813ee7f127659358d66b3919098cb68e3106f18
SupDex <symeton@gmail.com>
2020-09-12 20:58:34 +0300

    add routes for staffs and clients

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :clients, path: :clients, controllers: {
+    sessions: 'clients/sessions',
+    registrations: 'clients/registrations',
+    confirmations: 'clients/confirmations',
+    passwords: 'clients/passwords',
+  }
+
+  devise_for :staffs, path: :staffs, controllers: {
+    sessions: 'staffs/sessions',
+    registrations: 'staffs/registrations',
+    confirmations: 'staffs/confirmations',
+    passwords: 'staffs/passwords',
+  }
+
   root to: 'welcome#index'
 end
