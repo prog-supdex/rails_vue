72336e64efc7c3a4987afab826a9699c9422874f
SupDex <symeton@gmail.com>
2020-09-19 18:02:37 +0300

    add sass loader

---
+++ config/webpack/loaders/sass.js
@@ -0,0 +1,13 @@
+module.exports = {
+  test: /\.s[ac]ss$/i,
+  use: [
+    'vue-style-loader',
+    'css-loader',
+    {
+      loader: 'sass-loader',
+      options: {
+        implementation: require('sass')
+      }
+    }
+  ]
+}
