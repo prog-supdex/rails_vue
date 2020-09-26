cab7b4c4ba5b2704d4d21ee662cca71e86e9cd0e
SupDex <symeton@gmail.com>
2020-09-26 17:30:58 +0300

    remove console.log

--- app/javascript/packs/layouts/staff_navbar.js
+++ app/javascript/packs/layouts/staff_navbar.js
@@ -1,8 +1,6 @@
 import Vue from 'vue/dist/vue.js';
 import LayoutStaffNavbar from 'components/staff/layouts/navbar';
 
-console.log(LayoutStaffNavbar)
-
 new Vue({
   el: '#layouts-staff-navbar',
   components: {
