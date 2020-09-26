bbc6669ccc592ffc8efe952ea94563f16dfcca2d
SupDex <symeton@gmail.com>
2020-09-26 22:08:37 +0300

    remove text Hello Vue from app

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,17 +1,7 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
-}
 </script>
 
 <style lang="scss">
