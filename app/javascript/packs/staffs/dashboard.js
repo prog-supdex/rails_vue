5275c63121118688db6a97b3ff7764ae9314ce26
SupDex <symeton@gmail.com>
2020-09-28 21:04:02 +0300

    remove useless comment

--- app/javascript/packs/staffs/dashboard.js
+++ app/javascript/packs/staffs/dashboard.js
@@ -5,14 +5,6 @@ import axios from 'axios'
 document.addEventListener('DOMContentLoaded', () => {
   axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 
-  // Vue.component('client-list-component', {
-  //   render: h => h(Dashboard)
-  // })
-
-  // Vue.component('client-form-component', {
-  //   render: h => h(ClientForm)
-  // })
-
   new Vue({
     el: '#staff-clients',
     components: {
