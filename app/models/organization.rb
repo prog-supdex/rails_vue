57366b98e7f576801bfaeb489515011137cc01bb
SupDex <symeton@gmail.com>
2020-10-02 20:00:19 +0300

    add validates presence for fields organization. Add enum org_type

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,11 @@
 class Organization < ApplicationRecord
+  enum org_type: {
+    individual: 'individual',
+    juridical: 'juridical'
+  }
+
   has_many :organization_clients
   has_many :clients, through: :organization_clients
+
+  validates :name, :org_type, :inn, :ogrn, presence: true
 end
