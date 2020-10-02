86acc46b4ca5d07a9843f28a71e24ff12e4325fa
SupDex <symeton@gmail.com>
2020-10-02 19:55:09 +0300

    all require (not null) fields in organizations table

--- db/migrate/20201002164020_create_organizations.rb
+++ db/migrate/20201002164020_create_organizations.rb
@@ -1,10 +1,10 @@
 class CreateOrganizations < ActiveRecord::Migration[6.0]
   def change
     create_table :organizations do |t|
-      t.string :name
-      t.string :org_type
-      t.string :inn
-      t.string :ogrn
+      t.string :name, null: false
+      t.string :org_type, null: false
+      t.string :inn, null: false
+      t.string :ogrn, null: false
 
       t.timestamps
     end
