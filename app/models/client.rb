7d6a587d0732b0fd9047dd38487d76d1a229d13c
SupDex <symeton@gmail.com>
2020-10-03 15:03:31 +0300

    add dependent: :destroy to Client modal

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,14 @@
 class Client < ApplicationRecord
   include DeviseModules
 
-  has_many :organization_clients
+  has_many :organization_clients, dependent: :destroy
   has_many :organizations, through: :organization_clients
+
+  validates :name, :phone, :email, presence: true
+
+  def self.with_organization_ids
+    left_joins(:organization_clients)
+      .select("#{table_name}.*, array_agg(organization_clients.organization_id) AS organization_list_id")
+      .group("#{table_name}.id")
+  end
 end
