27fc5ce0c80ba1e544da4f196ce7d9a96699e2c1
SupDex <symeton@gmail.com>
2020-10-04 18:56:00 +0300

    include router to pack/staffs.js

--- app/javascript/packs/staff.js
+++ app/javascript/packs/staff.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import '../app/api/index'
-import Dashboard from "../app/components/staff/dashboard";
+import Dashboard from "app/components/staff/dashboard";
+import router from '../app/router'
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
+    router,
     render: h => h(Dashboard)
   }).$mount()
 })
