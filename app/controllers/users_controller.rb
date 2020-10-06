bd1adeae6780371878aaa1af51abfd68b61df644
SupDex <symeton@gmail.com>
2020-10-06 14:31:29 +0300

    add action to reset password of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,20 @@ class UsersController < ApplicationController
       render json: current_client.attributes.merge!(type: :client)
     end
   end
+
+  def reset_password
+    render json:
+       ChangePasswordByUserTypeCommand.call(
+        id: params[:id],
+        type: permitted_params[:type],
+        password: permitted_params[:password],
+        confirm_password: permitted_params[:confirm_password]
+      )
+  end
+
+  private
+
+  def permitted_params
+    params.permit(:type, :password, :confirm_password)
+  end
 end
